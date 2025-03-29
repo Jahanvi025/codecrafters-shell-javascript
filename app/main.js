fcf2ce3749b3ae5c7315d4f2f0a85c7c42cde1cc
Jahanvi Sharma <789jahanvi@gmail.com>
2025-03-29 22:48:00 +0530

    exit code apply

--- app/main.js
+++ app/main.js
@@ -10,8 +10,15 @@ const rl = readline.createInterface({
 function prompt() {
   rl.question("$ ", (answer) => {
     // Process the input, here it's always a 'command not found' message
-    console.log(`${answer}: command not found`);
-    prompt(); // Recursive call to continue prompting for input
+    if(answer.trim() === "exit 0"){
+      console.log("Exiting...");
+      rl.close(); // Close the readline interface
+      process.exit(0); // Exit the process with status 0
+    }
+    else{
+      console.log(`${answer}: command not found`);
+      prompt(); // Recursive call to continue prompting for input
+    }
   });
 }
 
